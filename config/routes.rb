5ccf2bfd5be4ff3b8943cd6a3ea64a6ab8c72f88
yurisi0212 <Dev.yurisi@gmail.com>
2021-10-01 11:04:04 +0900

    会員登録を実装(メールアドレスはまだない)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/index'
+  get 'pages/show'
+  root 'pages#index'
+
+  get 'pages/newprofile'
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
